ad4488e3d2a3def234e1845518ac066c3f281146
Thiago <thiagotavares2002@gmail.com>
2024-04-16 17:30:34 -0300

    feat: RepositoriesUserandTask

---
+++ src/main/java/com/thiago/todosimple/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.thiago.todosimple.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.thiago.todosimple.models.User;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+    
+}
